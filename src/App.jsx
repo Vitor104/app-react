70e8d801563e1f1a2366b834a571e0060f9685b1
Vitor104 <122707909+Vitor104@users.noreply.github.com>
2023-05-17 10:01:07 -0300

    dando os primeiros passos em React

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,16 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
+const MyName = 'Vitor';
+const MyNewName = MyName.toUpperCase();
 function App() {
 
   return (
-    
+    <div>
+      <h1>Olá, mundo!</h1>
+      <h2>E olá, {MyNewName}!</h2>
+    </div>
   );
-}
+};
 
 export default App;
